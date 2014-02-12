911fd707f1e469392d5ea249fe40476eadefb38f
fghfjujiber <xiaoming.chen10@gmail.com>
2014-02-12 18:01:48 +0000

    Redo make post

--- ItemAdvisor/ItemAdvisor/MakePostViewController.m
+++ ItemAdvisor/ItemAdvisor/MakePostViewController.m
@@ -14,16 +14,6 @@
 
 @implementation MakePostViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-        
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
