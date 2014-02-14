a780a95a2b7eb2049fc63867e611d7b1f1875309
fghfjujiber <xiaoming.chen10@gmail.com>
2014-02-14 10:30:17 +0000

    Make Post Back Button

---
+++ ItemAdvisor/ItemAdvisor/PrepareMakePostNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrepareMakePostNavigationViewController.h
+//  ItemAdvisor
+//
+//  Created by Xiaoming Chen on 14/02/2014.
+//  Copyright (c) 2014 ia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrepareMakePostNavigationViewController : UINavigationController
+
+@end
---
+++ ItemAdvisor/ItemAdvisor/PrepareMakePostNavigationViewController.m
@@ -0,0 +1,38 @@
+//
+//  PrepareMakePostNavigationViewController.m
+//  ItemAdvisor
+//
+//  Created by Xiaoming Chen on 14/02/2014.
+//  Copyright (c) 2014 ia. All rights reserved.
+//
+
+#import "PrepareMakePostNavigationViewController.h"
+
+@interface PrepareMakePostNavigationViewController ()
+
+@end
+
+@implementation PrepareMakePostNavigationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
