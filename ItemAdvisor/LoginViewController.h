939dc23f89aa7fea649b8f649c835a24c0ae32f7
fghfjujiber <xiaoming.chen10@gmail.com>
2014-02-19 16:01:13 +0000

    login view

---
+++ ItemAdvisor/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  ItemAdvisor
+//
+//  Created by Xiaoming Chen on 19/02/2014.
+//  Copyright (c) 2014 ia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *userName;
+@property (weak, nonatomic) IBOutlet UITextField *passWord;
+
+@end
---
+++ ItemAdvisor/LoginViewController.m
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.m
+//  ItemAdvisor
+//
+//  Created by Xiaoming Chen on 19/02/2014.
+//  Copyright (c) 2014 ia. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+- (IBAction)login:(id)sender {
+    
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
