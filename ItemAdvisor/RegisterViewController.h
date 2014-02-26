e56e21f972ba63cedd14fc2bd68603fd847e001f
fghfjujiber <xiaoming.chen10@gmail.com>
2014-02-26 17:19:16 +0000

    no message

--- ItemAdvisor/RegisterViewController.h
+++ ItemAdvisor/RegisterViewController.h
@@ -7,14 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
-//#import <AssetsLibrary/AssetsLibrary.h>
 #import "XWPhotoEditorViewController.h"
 #import "ImageFilterProcessViewController.h"
+#import "UserManager.h"
 
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 
-@interface RegisterViewController : UIViewController <UINavigationControllerDelegate,UIActionSheetDelegate,UIImagePickerControllerDelegate,XWFinishEditPhoto,ImageFitlerProcessDelegate>
+@interface RegisterViewController : UIViewController <UINavigationControllerDelegate,UIActionSheetDelegate,UIImagePickerControllerDelegate,XWFinishEditPhoto,ImageFitlerProcessDelegate,UserManagerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *profilePicButton;
 @property (strong, nonatomic) IBOutlet UIView *registerView;
