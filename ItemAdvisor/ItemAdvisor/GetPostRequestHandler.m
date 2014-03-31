af12e83eafd7fd283ce192cd7a8bea66d4ddf7d1
Yuxiang Zhou <mr.yuxiang.zhou@googlemail.com>
2014-03-31 12:46:59 +0100

    return post entity

--- ItemAdvisor/ItemAdvisor/GetPostRequestHandler.m
+++ ItemAdvisor/ItemAdvisor/GetPostRequestHandler.m
@@ -8,12 +8,42 @@
 
 #import "GetPostRequestHandler.h"
 #import "PostManager.h"
+#import "PostEntity.h"
 
 @implementation GetPostRequestHandler
 
 -(void)onSuccess:(NSDictionary *) jsonData{
     NSNumber *isSuccess = [NSNumber numberWithBool:[[jsonData objectForKey:@"result"]  isEqual: @"YES"]];
     NSMutableArray *posts = [NSMutableArray new];
+    
+    if([isSuccess boolValue]) {
+        NSArray *postlist = [jsonData objectForKey:@"posts"];
+        for (NSDictionary *post in postlist) {
+            PostEntity *postEntity = [[PostEntity alloc] init];
+            postEntity.postID = [[post objectForKey:@"id"] integerValue];
+            postEntity.NumberOfViews = [[post objectForKey:@"no_view"] integerValue];
+            postEntity.content = [post objectForKey:@"content"];
+            postEntity.timeStamp = [NSDate dateWithTimeIntervalSince1970:[[post objectForKey:@"time_stamp"] integerValue]];
+            
+            NSMutableArray *images = [NSMutableArray new];
+            NSMutableArray *tags = [NSMutableArray new];
+            
+            for (NSDictionary *img in [post objectForKey:@"imgs"]) {
+                [images addObject:[NSString stringWithFormat:@"http://113.55.0.233/itemadvisor/img/post/%@", [img objectForKey:@"img"]]];
+            }
+            
+            for (NSDictionary *tag in [post objectForKey:@"tags"]) {
+                [tags addObject:[[NSNumber alloc] initWithInteger:[[tag objectForKey:@"tag_type"] integerValue]]];
+            }
+            
+            postEntity.images = images;
+            postEntity.tags = tags;
+            
+            [posts addObject:postEntity];
+            
+        }
+    }
+    
     if([self.delegate respondsToSelector:@selector(onGetPost:content:)])
         [self.delegate performSelector:@selector(onGetPost:content:) withObject:isSuccess withObject:posts];
     
